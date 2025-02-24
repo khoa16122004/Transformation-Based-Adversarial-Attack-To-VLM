2d43ddbf4bd0653c1f3becc3780c400432c8f082
khoa16122004 <khoatr.work@gmail.com>
2025-02-24 23:00:29 +0700

    Update test_affine.py

--- src/test_affine.py
+++ src/test_affine.py
@@ -8,7 +8,7 @@ def apply_affine_transform(img, params):
     #     theta \in [-np.pi/6, np.pi/6]
     # """
     h, w = img.shape[:2]
-    
+    center_x, center_y = w / 2, h / 2
     [sx, sy, shx, shy, theta] = params
     affine_matrix = np.array([
         [sx * np.cos(theta) + shx * np.sin(theta), -sy * np.sin(theta) + shx * np.cos(theta), (1 - (sx * np.cos(theta) + shx * np.sin(theta))) * center_x + (sy * np.sin(theta) - shx * np.cos(theta)) * center_y],
