95da123597aa233d1db561c2b9c3f0f24c373a50
khoa16122004 <khoatr.work@gmail.com>
2025-02-24 23:00:00 +0700

    Update test_affine.py

--- src/test_affine.py
+++ src/test_affine.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-def apply_affine_transform(img, sx=1.0, sy=1.0, shx=0.0, shy=0.0, theta=0.0):
+def apply_affine_transform(img, params):
     # """
     #     s_x, s_y \in [0.8, 1.5]
     #     shx, shy \in [-0.2, 0.2]
@@ -9,8 +9,7 @@ def apply_affine_transform(img, sx=1.0, sy=1.0, shx=0.0, shy=0.0, theta=0.0):
     # """
     h, w = img.shape[:2]
     
-    center_x, center_y = w / 2, h / 2
-
+    [sx, sy, shx, shy, theta] = params
     affine_matrix = np.array([
         [sx * np.cos(theta) + shx * np.sin(theta), -sy * np.sin(theta) + shx * np.cos(theta), (1 - (sx * np.cos(theta) + shx * np.sin(theta))) * center_x + (sy * np.sin(theta) - shx * np.cos(theta)) * center_y],
         [sx * np.sin(theta) + shy * np.cos(theta),  sy * np.cos(theta) + shy * np.sin(theta), (1 - (sx * np.sin(theta) + shy * np.cos(theta))) * center_x + (-sy * np.cos(theta) - shy * np.sin(theta)) * center_y]
@@ -22,8 +21,7 @@ def apply_affine_transform(img, sx=1.0, sy=1.0, shx=0.0, shy=0.0, theta=0.0):
 
 img = cv2.resize(cv2.imread('images/lionsea.jpg'), (224, 224))
 cv2.imwrite("original.jpg", img)  
-transformed_img = apply_affine_transform(img, sx=1.42261337, sy=1.10903108 , 
-                                         shx= 0.0944826, shy= -0.14442866, 
-                                         theta=0.23890031)
+params = [1.1091362 ,  1.34097287,  0.09971993, -0.18609963, -0.07036193]
+transformed_img = apply_affine_transform(img, params)
 
 cv2.imwrite("output.jpg", transformed_img)  
