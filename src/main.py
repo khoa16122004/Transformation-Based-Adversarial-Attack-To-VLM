677fc0264de625d6bde69cdf801c30ca34992b6d
khoa16122004 <khoatr.work@gmail.com>
2025-02-24 22:36:50 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -31,13 +31,13 @@ if __name__ == "__main__":
                       c_gt,
                       )
     
-    func = fitness.IG_IT_fitness
     
     # [[0.8, 1.5], [0.8, 1.5], [-0.2, 0.2], [-0.2, 0.2], [-np.pi/6, np.pi/6]]
     bounds = [[0.8, 1.5], [0.8, 1.5], [-0.2, 0.2], [-0.2, 0.2], [-np.pi/6, np.pi/6]]
     
     best_solution, best_value, history, fitness_history = DE_vectorize(img, 
-                                                                       func=func, bounds=bounds,
+                                                                       func=fitness.IG_IT_fitness,
+                                                                       bounds=bounds,
                                                                        pop_size=args.pop_size,
                                                                        F=args.F,
                                                                        CR=args.CR,
